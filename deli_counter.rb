9ebdd5391de5a8f768fd466ec116293b595bd702
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 19:52:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 # Write your code here.
-def line(x)
-  if x.length == 0
+def line(array)
+  if array.length == 0
     puts "The line is currently empty."
   else
-    puts "There are #{x.length} people in line."
+    index = 0
+    str = "The line is currently: "
+    for i in array
+      str += "#{index + 1} #{array[index]} "
   end
 end
