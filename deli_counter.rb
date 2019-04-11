19f469b8ede399b04c63a7cb31cab4483ccdf872
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 20:01:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,14 @@ def take_a_number(line , name)
   return line.push(name)
 end
 
-def now_serving
+def now_serving(line)
+  if line.length > 0
+    str = "Now serving #{line[0]}!"
+    line.push
+    puts str
+  else
+    puts "There is nobody waiting to be served!"
+  end
+  
+    
 end
