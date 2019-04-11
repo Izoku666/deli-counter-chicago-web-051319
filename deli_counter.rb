dbb73d4f3d0ad3efbbb11b6986e71166aeb5cca0
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 19:50:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,6 @@ def line(x)
   if x.length == 0
     return "The line is empty."
   else
-    return "There are #{x.length} peoplein line."
+    return "There are #{x.length} people in line."
+  end
+end
